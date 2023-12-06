efb8430e0c8300d2ab1c3f82dcaa6c398ac6537e
Baxtiyar Azimov <baxtiyar.azimov@aahm.lan>
2023-12-06 11:54:04 +0400

    add

---
+++ main.js
@@ -0,0 +1,162 @@
+
+
+/*var marka =["Opel","Toyoto","Mazda","Renault","BMW"];
+var model =new Array ("Corsa","Yaris","CX-3","Clio");
+
+
+
+marka.unshift("HB");
+marka.push('blue');
+marka.pop();
+marka.shift();
+
+
+console.log(marka[1]);
+console.log(model[1]);
+console.log (marka[1] + " " + model[1]);
+console.log (marka[2] + " " + model[2]);
+console.log (marka[1] + " " + model[1]);
+console.log (marka[1] + " " + model[1]);
+
+// model [0]="Astra";
+
+// console.log(model);
+
+for(var i=0; i<marka.length; i++) {
+    console.log(marka[i]);
+}
+if(marka.indexOf('Mazda') !==-1) {
+
+    console.log ("gfgfgfgfgf");
+*/
+
+//   var now = 2018;
+//   var models =["Opel","Toyoto","Mazda","Renault","BMW"];
+//   var years =['2015','2012','2008','2023','1744'];
+
+//    for(var i=0; i<models.length; i++) {
+//     console.log(models[i]);
+//  }
+//    models.forEach(function(element){
+
+//    console.log(element);
+// });
+
+// for(var i=0; i<models.length; i++) {
+//          console.log(models[i] + ' '+years[i]+' model ve '
+//          + (now-years[i])+ ' yasinda masindir');
+//      }
+
+// var model = prompt("modele girin : ");
+// var year = prompt(" model ili girin : ");
+
+//   models.push(model);
+//   years.push(year);
+
+// for(var i=0; i<models.length; i++) {
+//            console.log(models[i] + ' '+years[i]+' model ve '
+//            + (now-years[i])+ ' yasinda masindir');
+//         }
+
+
+
+//    var marka = ['Opel','Toyota','Mazda'];
+//    var model = ['Astra','Prado','CX-9'];
+//    var il  =   ['2003','2008','2009'];
+//    var position = ['Land', 'Land of','Land dd', 'Landzz'];
+  
+  
+//    console.log(marka[0] + ' ' + model[0] + ' ' + il[0] + ' ' + position[0]);
+  
+
+// var opel ={
+//     marka:'Opel',
+//     model:'Corsa',
+//     il:'2003',
+//     reng:'mavi',
+//     avtomat:true
+// };
+
+// console.log(opel.model);
+// console.log(opel.avtomat);
+// console.log(opel['reng']);
+
+
+//     var toyota = new Object ();
+//    toyota.marka = 'Toyota';
+//    toyota.model = 'Prado';
+//    toyota.il ='2008';
+//    toyota.reng = 'yasil';
+//    toyota.avtomat =true;
+
+//    console.log(toyota.model);
+
+
+
+//    var arabalar = [
+//     {
+//         marka:'Opel',
+//         model:'Corsa',
+//         il:'2003',
+//         reng:'mavi',
+//         avtomat:true
+//     },
+
+//     {
+//         marka:'Mazda',
+//         model:'Corsa',
+//         il:'2008',
+//         reng:'divi',
+//         avtomat:true
+//     },
+
+//    ]
+
+//    console.log(arabalar);
+//    for(var i=0; i<arabalar.length; i++) {
+//     console.log(arabalar[i].marka);
+//    }
+
+
+// function topla (a,b) {
+//     return  a+b;
+// }
+//  var sonda = topla(10,20);
+ 
+//  console.log(sonda);
+//  console.log(topla(40,20))
+
+
+// var now = 2023;
+
+// function yashesapla(yas) {
+// return now-yas;
+// }
+// var ali =yashesapla(1989);
+// var azi =yashesapla(1999);
+
+// console.log(ali);
+
+
+
+// function faktorial (say) {
+//     let yekun =1;
+//     for( let i=2; i<=say; i++){
+//         yekun *=i;
+//     }
+//     return yekun;
+// }
+// console.log(faktorial(7));
+
+
+// var isblue = false;
+// var blue = document.getElementById('goy');
+
+// setInterval(function(){
+//     if(isblue) {
+//         blue.style.background='green';
+//     }else{
+//         blue.style.background='blue';
+//     }
+//     isblue=!isblue;
+// },1000);
